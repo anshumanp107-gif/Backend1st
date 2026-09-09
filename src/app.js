dc6797784c25e1e835732fb5862c15d32121d0f0
Anshuman Pandey <anshumanp107@gmail.com>
2026-09-09 22:31:01 +0530

    bcrypt inplemented

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const bcrypt = require("bcrypt");
 const path = require('path');
+const userModel = require("./models/user")
+const postModel = require("./models/post");
+const { hash } = require('crypto');
 
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -18,6 +22,23 @@ app.get("/login",(req,res)=>{
 
 app.get("/signUp",(req,res)=>{
     res.render("signUp")
+})
+
+app.post("/signUp",async (req,res)=>{
+    let {name,email,gender,password} = req.body;
+    let isUserExist = await userModel.findOne({email})
+    if(isUserExist) return res.send("User has been created by this emailId");
+    bcrypt.genSalt(10,(err,salt)=>{
+        bcrypt.hash(password,salt,(err,hash)=>{
+            userModel.create({
+                name:name,
+                email:email,
+                gender:gender,
+                password:hash
+            })
+        })
+    })
+    res.send("User created Succesfully")
 })
 
 app.get("/profile",(req,res)=>{
